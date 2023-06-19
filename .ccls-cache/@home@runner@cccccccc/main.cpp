b96a9dde9e667f30cdb21b2e791413eae18343f6
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-06-19 04:19:10 +0000

    ee

--- .ccls-cache/@home@runner@cccccccc/main.cpp
+++ .ccls-cache/@home@runner@cccccccc/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int max_mul;
+
+int count_mul(int n) { return log(n) / log(3); }
+
+int solve(int add, int mul, int n) {
+  int result = 0;
+
+  if (2 * max_mul < add) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+
+  if (n % 3 == 0) {
+    if (2 * (mul + 1) <= add) {
+      result += solve(add, mul + 1, n / 3);
+    }
+
+    result += solve(add + 3, mul, n - 3);
+  } else if (n % 3 != 0) {
+    result += solve(add + (n % 3), mul, n - (n % 3));
+  }
+
+  return result;
+}
+
+int solution(int n) {
+  int answer = 0;
+
+  max_mul = count_mul(n);
+  answer = solve(0, 0, n);
+
+  return answer;
 }
