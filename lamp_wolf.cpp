f61749830452563388a286466c12a32ca96b3af2
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-07-24 09:39:11 +0000

    e

--- lamp_wolf.cpp
+++ lamp_wolf.cpp
@@ -1,45 +1,28 @@
 #include <algorithm>
-#include <string>
+#include <set>
 #include <vector>
 using namespace std;
-
-int answer = 1;
-int s = 0;
-int w = 0;
-
-void dfs(int curr_idx, int w, int s, vector<int> nextNode, vector<int> info,
-         vector<vector<int>> v) {
-  int animal = info[curr_idx];
-  if (!animal)
-    s++;
+int N, ans;
+vector<int> adj[17], info;
+void sol(int n, set<int> s, int a, int b) {
+  if (info[n])
+    b++;
   else
-    w++;
-
-  answer = max(answer, s);
-
-  if (w >= s)
+    a++;
+  if (b >= a)
     return;
-
-  for (int i = 0; i < nextNode.size(); i++) {
-    vector<int> next = nextNode;
-    next.erase(next.begin() + i);
-    for (int j = 0; j < v[nextNode[i]].size(); j++)
-      next.push_back(v[nextNode[i]][j]);
-    dfs(nextNode[i], w, s, next, info, v);
-  }
+  s.erase(n);
+  ans = max(a, ans);
+  for (auto nxt : adj[n])
+    s.insert(nxt);
+  for (auto v : s)
+    sol(v, s, a, b);
 }
-
-int solution(vector<int> info, vector<vector<int>> edges) {
-  vector<vector<int>> v(info.size());
-
-  for (int i = 0; i < edges.size(); i++)
-    v[edges[i][0]].push_back(edges[i][1]);
-
-  vector<int> nextNode; // 0번 노드와 연결된 값부터 시작하기 위해서
-  for (int i = 0; i < v[0].size(); i++)
-    nextNode.push_back(v[0][i]);
-
-  dfs(0, 0, 0, nextNode, info, v);
-
-  return answer;
+int solution(vector<int> _info, vector<vector<int>> edges) {
+  N = info.size();
+  info = _info;
+  for (auto eg : edges)
+    adj[eg[0]].push_back(eg[1]);
+  sol(0, {0}, 0, 0);
+  return ans;
 }
