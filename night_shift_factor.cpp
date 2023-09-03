0be2cd2c990a5056d03535c26511fd264bc65c48
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-09-03 08:12:40 +0000

    e

---
+++ night_shift_factor.cpp
@@ -0,0 +1,22 @@
+#include <queue>
+#include <string>
+#include <vector>
+using namespace std;
+long long solution(int n, vector<int> works) {
+  long long answer = 0;
+  priority_queue<int> q;
+  int number;
+  for (auto x : works)
+    q.push(x);
+  while (n-- && !q.empty()) {
+    number = q.top();
+    q.pop();
+    if (number != 1)
+      q.push(number - 1);
+  }
+  while (!q.empty()) {
+    answer += (q.top() * q.top());
+    q.pop();
+  }
+  return answer;
+}
